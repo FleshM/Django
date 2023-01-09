d1e0e10176b38d08210feff98f9c328a0e9d97d8
Denis <d.pleshivtsev1@gmail.com>
2023-01-09 15:42:53 +0500

    init

---
+++ app1/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from app1.models import Profession
+
+
+def index_page(request):
+    data = {
+        'profession': Profession.objects.get(id=1)
+    }
+    return render(request, 'index.html', context=data)
